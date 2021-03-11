4d52e0c42012defef45563e8e464098a117ba20f
XRaiderGH <raigriffioen3@gmail.com>
2021-03-11 13:56:25 +0100

    veranderingen layout

--- robin-dashboard/components/layout.js
+++ robin-dashboard/components/layout.js
@@ -1,12 +1,18 @@
 const Layout = ({children}) => {
     return (
         <>
-            <div className={'nav'}>
-
+            <div className='flex'>
+                <nav className='w-328'>
+                    <ul>
+                        <li>test</li>
+                        <li>test</li>
+                        <li>test</li>
+                    </ul>
+                </nav>
+                <main className='bg-gradient-to-br from-green-500 to-blue-400 from h-100 w-full'>
+                    {children}
+                </main>
             </div>
-            <main>
-                {children}
-            </main>
         </>
     )
 };
--- robin-dashboard/tailwind.config.js
+++ robin-dashboard/tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'green-500' : '#6DBD98',
+        'blue-500' : '#5B99D2'
+      },
+      width: {
+        '328' : '328px'
+      }
+    },
   },
   variants: {
     extend: {},
