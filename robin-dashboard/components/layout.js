5608edfe9719da78fed174101b4b07d485e9ce91
XRaiderGH <raigriffioen3@gmail.com>
2021-03-11 13:13:11 +0100

    tailwind toegevoegd en begin gemaakt aan layout component

---
+++ robin-dashboard/components/layout.js
@@ -0,0 +1,14 @@
+const Layout = ({children}) => {
+    return (
+        <>
+            <div className={'nav'}>
+
+            </div>
+            <main>
+                {children}
+            </main>
+        </>
+    )
+};
+
+export default Layout
