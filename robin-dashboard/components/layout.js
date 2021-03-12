c0448ffe58a3b1358ff80ccd07f2cc246ae6bc53
Rai Griffioen <raigriffioen3@gmail.com>
2021-03-12 12:36:13 +0100

    Layout afgerond

--- robin-dashboard/components/layout.js
+++ robin-dashboard/components/layout.js
@@ -1,27 +1,68 @@
+import Header from './header'
+import {useRouter} from 'next/router'
+import Link from './ActiveLink'
 import Image from 'next/image'
+
 const Layout = ({children}) => {
+
+    const router = useRouter();
     return (
         <>
             <div className='flex'>
-                <nav className='w-328'>
-                    <ul>
-                        <li>test</li>
-                        <li>test</li>
-                        <li>test</li>
-                    </ul>
-                </nav>
+                <div className="sideNav">
+                    <div className="flex flex-col text-xl mt-8 ml-8 mb-8">
+                        <span>Logged in as,</span>
+                        <span>Sjoerd Roelvink</span>
+                    </div>
+
+                    <nav className='w-328'>
+                        <ul className='navItems text-3xl font-medium'>
+                            <li>
+                                <Link href={'/'}>
+                                    <a className='navItem block w-100 pl-7 py-8'>
+                                        <Image src='/assets/tachometer-alt-solid.svg' width='29px' height='23px' alt=""/>
+                                        <span className='ml-5'>Overview</span>
+                                    </a>
+                                </Link>
+                                <hr/>
+                            </li>
+
+                            <li>
+                                <Link href={'/clients'}>
+                                    <a className='navItem block w-100 pl-7 py-8'>
+                                        <Image src='/assets/users-solid.svg' width='29px' height='23px' alt=""/>
+                                        <span className='ml-5'>Clients</span>
+                                    </a>
+                                </Link>
+                                <hr/>
+                            </li>
+
+                            <li>
+                                <Link href={'/tasks'}>
+                                    <a className='navItem block w-100 pl-7 py-8'>
+                                        <Image src='/assets/tasks-solid (2).svg' width='29px' height='23px' alt=""/>
+                                        <span className='ml-5'>Tasks</span>
+                                    </a>
+                                </Link>
+                                <hr/>
+                            </li>
+
+                            <li>
+                                <Link href={'/panic'}>
+                                    <a className='navItem block w-100 pl-7 py-8'>
+                                        <Image src='/assets/bell-solid.svg' width='29px' height='23px' alt=""/>
+                                        <span className='ml-5'>Panic</span>
+                                    </a>
+                                </Link>
+                                <hr/>
+                            </li>
+
+                        </ul>
+                    </nav>
+                </div>
                 <main className='bg-gradient-to-br from-green-500 to-blue-400 from h-100 w-full flex flex-row justify-center'>
-                    <div className="wrap w-80">
-                        <div className="header mt-7 mb-7">
-                            <div className="flex flex-row justify-between">
-                                <div className="logo"><Image src="/assets/logo.svg" width='130' height='66' alt="Logo"/></div>
-                                <div className="flex flex-col justify-center">
-                                    <button className="sign-out text-xl text-white border-2 border-white rounded-3xl w-186 h-50 py-1 transition duration-200 ease-in-out hover:bg-blue-500">
-                                        Sign out
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
+                    <div className="wrap sm:w-9/10 w-80">
+                        <Header/>
                         <hr className='opacity-20'/>
                         <div className="body mt-7">
                             {children}
