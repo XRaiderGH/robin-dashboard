349f20b3a06767adf156c847461fe70fa0d92070
Sam Harwig <42715585+25530@users.noreply.github.com>
2021-03-12 14:00:48 +0100

    Begin overview

--- robin-dashboard/pages/index.js
+++ robin-dashboard/pages/index.js
@@ -5,7 +5,8 @@ import Layout from '../components/layout';
 export default function Home() {
   return (
    <Layout>
-     <h1>test</h1>
+     <h1 class="overviewcap">Hi Sjoerd,</h1>
+     <p class="overviewpara">There are 3 clients with expired tasks and 5 rapidly emerging tasks</p>
    </Layout>
   )
 }
