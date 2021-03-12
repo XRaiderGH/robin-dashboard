e2e75206d993c6a5a757f12e31e671796057ad28
Sam Harwig <42715585+25530@users.noreply.github.com>
2021-03-12 17:21:34 +0100

    Overviewafvoornu

--- robin-dashboard/pages/index.js
+++ robin-dashboard/pages/index.js
@@ -35,21 +35,30 @@ export default function Home() {
        </div>
        
        <div class="taskbox">
-        <h2 class="taskhead">Expired tasks</h2>
+        <h2 class="taskheading">Rapidly emerging tasks</h2>
+        <p class="taskparagr">Upcoming tasks for the next 2 hours</p>
         <div class="tasksinfor">
           <img class="taskoverimg" src='/assets/tijdelijklogo.JPG' alt=""/>
-          <div class="tasktext"><h3 class="taskhead2">Esther Dobson</h3><p class="paragr">1 hour and 13 expired</p>
+          <div class="tasktext"><h3 class="taskhead2">William Whatley</h3><p class="paragr">In 23 min</p>
           </div>
           <img class="taskoverimg" src='/assets/tijdelijklogo.JPG' alt=""/>
-          <div class="tasktext"><h3 class="taskhead2">William Whatley</h3><p class="paragr">1 hour and 13 expired</p>
+          <div class="tasktext"><h3 class="taskhead2">Esther Dobson</h3><p class="paragr">in 48 min</p>
           </div>
           <img class="taskoverimg" src='/assets/tijdelijklogo.JPG' alt=""/>
-          <div class="tasktext"><h3 class="taskhead2">Roy Gleason</h3><p class="paragr">1 hour and 13 expired</p>
+          <div class="tasktext"><h3 class="taskhead2">Roy Gleason</h3><p class="paragr">1 hour and 28 min</p>
+          </div>
+          <img class="taskoverimg" src='/assets/tijdelijklogo.JPG' alt=""/>
+          <div class="tasktext"><h3 class="taskhead2">Marryrose L Salas</h3><p class="paragr">1 hour and 45 min</p>
+          </div>
+          <img class="taskoverimg" src='/assets/tijdelijklogo.JPG' alt=""/>
+          <div class="tasktext"><h3 class="taskhead2">Kenneth Johnson</h3><p class="paragr">1 hour and 58 min</p>
           </div>
         </div>
         <div class="tasksinfor2">
            <button class="buttonoverview">Brushing teeth</button>
            <button class="buttonoverview">Take medication</button>
+           <button class="buttonoverview">Brushing teeth</button>
+           <button class="buttonoverview">Laundry</button>
            <button class="buttonoverview">Brushing teeth</button>
         </div>
           <div class="buttondiv">
