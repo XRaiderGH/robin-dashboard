c7db7f706d990c7afda2e076d724c32cbd325082
Rai Griffioen <raigriffioen3@gmail.com>
2021-03-12 12:51:54 +0100

    pagina's aangemaakt en layout verandering

--- robin-dashboard/pages/_app.js
+++ robin-dashboard/pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/globals.css';
 import '../styles/styles.css';
 import "tailwindcss/tailwind.css";
+import Layout from "../components/layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+  )
 }
 
 export default MyApp
--- robin-dashboard/pages/index.js
+++ robin-dashboard/pages/index.js
@@ -4,8 +4,6 @@ import Layout from '../components/layout';
 
 export default function Home() {
   return (
-   <Layout>
      <h1>test</h1>
-   </Layout>
   )
 }
