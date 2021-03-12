0c8a6690609e216e37f67bd010972266090d7a1f
Sam Harwig <42715585+25530@users.noreply.github.com>
2021-03-12 15:27:39 +0100

    Begin expired tasks

--- robin-dashboard/pages/index.js
+++ robin-dashboard/pages/index.js
@@ -7,6 +7,18 @@ export default function Home() {
    <Layout>
      <h1 class="overviewcap">Hi Sjoerd,</h1>
      <p class="overviewpara">There are 3 clients with expired tasks and 5 rapidly emerging tasks</p>
+
+     <div class="taskoverview">
+       <div class="taskbox">
+        <h2 class="taskhead">Expired tasks</h2>
+        <div class="tasksinfor">
+          <img class="taskoverimg" src='/assets/tijdelijklogo.JPG' width='29px' height='23px' alt=""/>
+          <div class="tasktext"><h3 class="taskhead2">Esther Dobson</h3><p class="paragr">1 hour and 13 expired</p>
+          </div>
+          <button class="buttonoverview">Brushing teeth</button>
+        </div>
+       </div>
+     </div>
    </Layout>
   )
 }
