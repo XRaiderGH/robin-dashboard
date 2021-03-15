3a4e4de67308818014b29a6161ac41c358a3f1eb
XRaiderGH <raigriffioen3@gmail.com>
2021-03-15 11:20:50 +0100

    client detail

--- robin-dashboard/pages/clients/client/index.js
+++ robin-dashboard/pages/clients/client/index.js
@@ -3,13 +3,12 @@ import Image from 'next/image'
 
 export default function index() {
     return (
-        <div className='container w-4/6 bg-white rounded-3xl shadow-custom px-12 py-10 bg-opacity-90'>
+        <div className='container w-100 bg-white rounded-3xl shadow-custom px-12 py-10 bg-opacity-90'>
             <div className="top flex flex-row justify-between pb-10">
-                <span className='flex flex-col justify-center'><h1 className='font-medium text-3xl'>Client 22323</h1></span>
-                <span className='border-3 px-3 rounded-3xl border-blue-500 py-2 flex flex-row'>
-                         <Image src='/assets/search-solid.svg' width='16.5' height='16.5'/>
-                         <input type="text" className='ml-2 focus:outline-none placeholder-blue-500 font-bold w-52 bg-transparent text-lg' placeholder='Search'/>
-                    </span>
+                    <div>
+                        <h1 className='font-medium text-3xl'>William Whatley</h1>
+                        <span className='font-medium text-gray-800'>Client Tasks</span>
+                    </div>
             </div>
 
             <div className="bottom">
